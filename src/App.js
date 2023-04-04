d261e2df1e50950b79580021f5467a4dc8281761
Rofael-Alfons <rofa.alfons7@gmail.com>
2023-04-04 21:00:47 +0200

    edited some configs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { Fragment } from "react";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
+import Account from "./Pages/Account/Account";
 import Discover from "./Pages/Discover/Discover";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Discover></Discover>
-      <Footer></Footer>
-    </div>
+    <Fragment>
+      <div className="main">
+        <Navbar></Navbar>
+        <Discover></Discover>
+        <Footer></Footer>
+      </div>
+      <div className="account">
+        <Account />
+      </div>
+    </Fragment>
   );
 }
 
